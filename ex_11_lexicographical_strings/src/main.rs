7b9607a1c80f685a829a46bcd009821d3023596f
Samu <sam_chacla@hotmail.com>
2022-04-18 09:22:09 -0500

    nueva version para la solucion del ultimo algoritmo

--- ex_11_lexicographical_strings/src/main.rs
+++ ex_11_lexicographical_strings/src/main.rs
@@ -1,3 +1,5 @@
+// Obtener una lista ordenada de palabras que existen dentro de otro grupo de palabras
+// ejem: a = ["xyz", "live", "strong"]  b="lively" ... entonces 'live' será valido por existe en 'lively'
 fn main() {
     let result = in_array(
         &["xyz", "live", "strong"],
@@ -9,13 +11,43 @@ fn main() {
 fn in_array(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
 
+    let xx = arr_a
+        .iter()
+        .filter(|&a| arr_b.iter().any(|&b| b.contains(a)));
+
+    println!("{:?}", xx);
+
+    result.sort();
+    result.dedup();
+    return result;
+}
+
+fn in_array_version_b(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
+    for b in arr_b {
+        let mut res = arr_a
+            .iter()
+            .filter(|&a| b.contains(a))
+            .map(|&m| m.to_string())
+            .collect::<Vec<_>>();
+        result.append(&mut res);
+    }
+    result.sort();
+    result.dedup();
+    return result;
+}
+
+fn in_array_version_c(arr_a: &[&str], arr_b: &[&str]) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
     for b in arr_b {
         for a in arr_a {
             let contained = b.contains(a);
             if contained {
-                let ss = a.to_string();
-                if result.contains(&ss) == false {
-                    result.push(a.to_string());
+                let new_word = a.to_string();
+                if result.contains(&new_word) == false {
+                    result.push(new_word);
                 }
             }
         }
